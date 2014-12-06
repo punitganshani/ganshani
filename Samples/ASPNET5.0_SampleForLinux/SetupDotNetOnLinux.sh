42c3ce7c8597bfb53d7a966bde9a6f61f58840ad
Punit Ganshani <punit.ganshani@gmail.com>
2014-12-06 21:33:30 +0800

    Added libuv
    Added libuv - thanks to David Fowler (@davidfowl for pointing it out)

--- Samples/ASPNET5.0_SampleForLinux/SetupDotNetOnLinux.sh
+++ Samples/ASPNET5.0_SampleForLinux/SetupDotNetOnLinux.sh
@@ -38,6 +38,14 @@ sudo certmgr -ssl -m https://www.myget.org/F/aspnetvnext/
 
 mozroots --import –sync
 
+wget http://dist.libuv.org/dist/v1.0.0-rc1/libuv-v1.0.0-rc1.tar.gz 
+tar -xvf libuv-v1.0.0-rc1.tar.gz
+cd libuv-v1.0.0-rc1/
+./gyp_uv.py -f make -Duv_library=shared_library
+make -C out
+sudo cp out/Debug/lib.target/libuv.so /usr/lib/libuv.so.1.0.0-rc1
+sudo ln -s libuv.so.1.0.0-rc1 /usr/lib/libuv.so.1
+
 curl -sSL https://raw.githubusercontent.com/aspnet/Home/master/kvminstall.sh | sh & source ~/.kre/kvm/kvm.sh
 
 source ~/.kre/kvm/kvm.sh
