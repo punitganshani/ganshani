3c6f0414647459c4fc169734070c45f16ed82a9f
Punit Ganshani <punit.ganshani@gmail.com>
2015-02-22 18:53:04 +0800

    Self hosted Owin Example

---
+++ Samples/OwinHostedWebsite/OwinWeb/OwinWeb/Program.cs
@@ -0,0 +1,34 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwinWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Microsoft.Owin.Hosting.WebApp.Start<OwinSampleStartup>("http://localhost:2259"))
+            {
+                Console.WriteLine("Open the address in the browser or");
+                Console.WriteLine("Press any key to continue..");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class OwinSampleStartup
+    {
+        public void Configuration(IAppBuilder sampleapp)
+        {
+            sampleapp.Run(sample =>
+            {
+                sample.Response.ContentType = "text/plain";
+                return sample.Response.WriteAsync("Hello from OWIN");
+            });
+        }
+    }
+}
